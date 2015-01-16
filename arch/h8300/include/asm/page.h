3d80a941dbe93d8c2f32f9f3a6d3269b0f61169c
Yoshinori Sato <ysato@users.sourceforge.jp>
2015-01-17 03:52:05 +0900

    remove garbege

--- arch/h8300/include/asm/page.h
+++
@@ -1,9 +0,0 @@
-#ifndef _H8300_PAGE_H
-#define _H8300_PAGE_H
-
-#include <asm-generic/page.h>
-#include <linux/types.h>
-
-#define MAP_NR(addr) (((uintptr_t)(addr)-PAGE_OFFSET) >> PAGE_SHIFT)
-
-#endif
