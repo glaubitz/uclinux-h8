a165ad1fb96a5398868c37f3fbeee3aee245c917
Yoshinori Sato <ysato@users.sourceforge.jp>
2022-01-28 00:37:53 +0900

    rx: Add ARCH_PFN_OFFSET.
    Signed-off-by: Yoshinori Sato <ysato@users.sourceforge.jp>

--- arch/rx/include/asm/page.h
+++ arch/rx/include/asm/page.h
@@ -2,18 +2,15 @@
 #ifndef _RX_PAGE_H
 #define _RX_PAGE_H
 
+#ifndef __ASSEMBLY__
+extern unsigned int rx_ram_pfn_base;
+#define ARCH_PFN_OFFSET (rx_ram_pfn_base)
+#endif
 #include <asm-generic/page.h>
 #include <linux/types.h>
 
 #define MAP_NR(addr) (((uintptr_t)(addr)-PAGE_OFFSET) >> PAGE_SHIFT)
 #define VM_DATA_DEFAULT_FLAGS	(VM_READ | VM_WRITE | VM_EXEC | \
 				 VM_MAYREAD | VM_MAYWRITE | VM_MAYEXEC)
-
-#ifndef __ASSEMBLY__
-extern unsigned long rom_length;
-extern unsigned long memory_start;
-extern unsigned long memory_end;
-extern unsigned long _ramend;
-#endif
 
 #endif
