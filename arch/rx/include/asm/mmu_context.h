07c6e6e94fe592dd533ff596084f5d10d40ff97e
Yoshinori Sato <yo-satoh@sios.com>
2022-01-28 21:35:10 +0900

    rx: add missing headers.
    Signed-off-by: Yoshinori Sato <yo-satoh@sios.com>

---
+++ arch/rx/include/asm/mmu_context.h
@@ -0,0 +1,6 @@
+#ifndef _ASM_RX_MMU_CONTEXT_H
+#define _ASM_RX_MMU_CONTEXT_H
+
+#include <asm-generic/nommu_context.h>
+
+#endif /* _ASM_RX_MMU_CONTEXT_H */
---
+++ arch/rx/include/asm/vmalloc.h
@@ -0,0 +1,4 @@
+#ifndef _ASM_RX_VMALLOC_H
+#define _ASM_RX_VMALLOC_H
+
+#endif /* _ASM_RX_VMALLOC_H */
